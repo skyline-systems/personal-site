a6960ceb5379cdb9adaa2ca79e632065aa9fdbab
Sahibjot <sahibjot@live.ca>
2025-04-05 07:33:02 -0400

    change footer scrolling text

--- app/footer.tsx
+++ app/footer.tsx
@@ -73,7 +73,7 @@ export function Footer() {
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© {new Date().getFullYear()} Sahibjot Saggu.</span>
-            <span>Built with Motion-Primitives.</span>
+            <span>Built with ❤️</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
