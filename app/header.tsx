ed8a7c08da34321952ac65909041939649a80cc5
Sahibjot Singh <sahibjot@live.ca>
2025-11-27 02:05:16 -0500

    update subtitle

--- app/header.tsx
+++ app/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
             className="text-base tracking-tighter text-zinc-600 dark:text-zinc-500"
             delay={0.5}
           >
-            Product-focused frontend engineer
+            Product-focused software engineer
           </TextEffect>
         </div>
       </div>
